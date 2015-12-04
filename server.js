353946510e897a288a9b30c2ae1a8b64bb2466a5
isangieri <isabelleangieri@gmail.com>
2015-12-04 10:05:37 -0800

    added a function to add CORS to the header of the origin

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var express = require('express'),
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
 
+// Allow CORS: we'll use this today to reduce security so we can more easily test our code in the browser.
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 /************
  * DATABASE *
  ************/
