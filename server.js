c2e6496d5a15f90e6461b027ef0ea2e835f7e6f3
theletterrsquared@gmail.com <theletterrsquared@gmail.com>
2015-12-04 10:05:32 -0800

    allowing u access

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ app.use(express.static(__dirname + '/public'));
 /**********
  * ROUTES *
  **********/
-
+// Allow CORS: we'll use this today to reduce security so we can more easily test our code in the browser.
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 /*
  * HTML Endpoints
  */
