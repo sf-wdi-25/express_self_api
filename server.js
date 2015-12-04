36209f47a4c0ae59cfb0f7cceec32d9e4e1d2805
chan-d <chan.danny516@gmail.com>
2015-12-04 10:05:28 -0800

    added middleware

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var express = require('express'),
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 /************
  * DATABASE *
  ************/
