53cb0f9f691bbb853497219be6eb274631349aa1
Joe Roers <Joe@Joe-Roers-MacBook-Pro.local>
2015-12-04 09:52:39 -0800

    updated documentation_url and base_url in server.js

--- server.js
+++ server.js
@@ -31,12 +31,12 @@ app.get('/', function homepage (req, res) {
 app.get('/api', function api_index (req, res){
   res.json({
     message: "Welcome to my personal api!",
-    documentation_url: "https://github.com/sf-wdi-25/express_self_api/README.md", // CHANGE THIS TO LINK TO YOUR README.md
-    base_url: "http://YOUR-APP-NAME.herokuapp.com",
+    documentation_url: "https://github.com/jroers/express_self_api/README.md", // CHANGE THIS TO LINK TO YOUR README.md
+    base_url: "http://damp-retreat-5020.herokuapp.com",
     endpoints: [
       {method: "GET", path: "/api", description: "Describes available endpoints"}
     ]
-  })
+  });
 });
 
 /**********
