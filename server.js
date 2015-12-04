aaea675516e31c11ee7f5849c9556b297f45905e
isangieri <isabelleangieri@gmail.com>
2015-12-04 09:52:26 -0800

    changed the documentation and base urls in server.js to reflect isangieri github and my heroku site

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.get('/', function homepage (req, res) {
 app.get('/api', function api_index (req, res){
   res.json({
     message: "Welcome to my personal api!",
-    documentation_url: "https://github.com/sf-wdi-25/express_self_api/README.md", // CHANGE THIS TO LINK TO YOUR README.md
-    base_url: "http://YOUR-APP-NAME.herokuapp.com",
+    documentation_url: "https://github.com/isangieri/express_self_api/README.md", // CHANGE THIS TO LINK TO YOUR README.md
+    base_url: "http://gentle-shore-4526.herokuapp.com",
     endpoints: [
       {method: "GET", path: "/api", description: "Describes available endpoints"}
     ]
