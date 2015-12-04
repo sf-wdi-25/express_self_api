09608646dc4435464e980d1dbf1de3221ceb7f54
nathanallen <thenathanator@gmail.com>
2015-12-03 21:29:11 -0800

    stub out server js

--- server.js
+++ server.js
@@ -1,23 +1,47 @@
-// server.js
-// SERVER-SIDE JAVASCRIPT
+// require express and other modules
+var express = require('express'),
+    app = express();
 
-//require express in our app
-var express = require('express');
-// generate a new express app and call it 'app'
-var app = express();
+// serve static files from public folder
+app.use(express.static(__dirname + '/public'));
 
-// serve static files in public
-// your code here
+/************
+ * DATABASE *
+ ************/
 
+// your hardcoded data here
 
-// define a root route: localhost:3000/
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+/**********
+ * ROUTES *
+ **********/
+
+/*
+ * HTML Endpoints
+ */
+
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 
+/*
+ * JSON API Endpoints
+ */
 
+app.get('/api', function api_index (req, res){
+  res.json({
+    message: "Welcome to my personal api!",
+    endpoints: [
+      {method: "GET", path: "/api", description: "Describes available endpoints"}
+    ]
+  })
+});
 
+/**********
+ * SERVER *
+ **********/
+
+// listen on port 3000
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening at http://localhost:3000/');
+  console.log('Express server is running on http://localhost:3000/');
 });
