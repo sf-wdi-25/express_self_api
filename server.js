4a1969e78b9867447795c69be9c707510cbbd665
tscurrie <tscurrie85@gmail.com>
2015-12-04 10:05:54 -0800

    allow cors

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ app.use(express.static(__dirname + '/public'));
  ************/
 
 // your hardcoded data here
-
+ // Allow CORS: we'll use this today to reduce security so we can more easily test our code in the browser.
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 /**********
  * ROUTES *
  **********/
