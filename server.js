60b0366255717778218f658e7677e7a96073f6b9
anonym0us3 <nathan.shayevich@gmail.com>
2015-12-04 10:05:29 -0800

    added CORS functionality

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 // require express and other modules
 var express = require('express'),
     app = express();
+    
+	// Allow CORS: we'll use this today to reduce security so we can more easily test our code in the browser.
+	app.use(function(req, res, next) {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		next();
+	});
 
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
