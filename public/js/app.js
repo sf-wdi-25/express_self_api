d5d279dd4bf52d605e369b249e3ed69fa9c2ebb7
trebloc <ctse77@alum.berkeley.edu>
2015-12-06 12:09:20 -0800

    updated JS, Index Page

--- public/js/app.js
+++ public/js/app.js
@@ -4,4 +4,20 @@ $(document).ready(function(){
 
 // your code
 
+$.ajax({
+         method: 'GET',
+         url: 'http://localhost:3000/api/profile',
+         success: function(data) {
+         	data.forEach(function (element){
+         		$('#profile').append("<p>" + element.name + "</p>")
+
+         
+         })
+       },  	
+         error: function() { console.log('uh oh')
+
+         }
+    });
+
+});
 });
