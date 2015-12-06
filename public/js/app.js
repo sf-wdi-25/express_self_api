fbbd7f89cf78b5681090345b8438c5b47e6f82f6
Zackajavach <jdelora17@gmail.com>
2015-12-06 12:36:15 -0800

    somewhere there

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,14 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-// your code
-	
+// // your code
+// 	$.ajax ({
+// 		method:'GET',
+// 		url:'http://localhost:3000/api/profile',
+// 		success: function(data) {
+// 			data.forEach(function (element) {
+// 		}
+// 	})
+// 	})
+		
 
