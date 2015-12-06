7ba329d2d085c7fce5fc23c09ed2a0426f59200c
kehontas <kehontas@gmail.com>
2015-12-06 14:28:25 -0800

    checking for info

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,20 @@
-console.log("Sanity Check: JS is working!");
-
 $(document).ready(function(){
 
-// your code
+	console.log("Sanity Check: My JS is working!");
 
+
+
+	$.ajax({
+  	method: "GET",
+	url: "https://super-crud.herokuapp.com/api/profile",
+  	success: function (data) {
+   
+	function render(info) {
+    	messages.forEach(function (info){
+       	$("div#target").append("<p>" + message.message + "</p>");
+    	});
+  	}
+  	render(response.data);
+	}
+});
 });
