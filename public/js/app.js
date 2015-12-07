65eaa46fcb7f174265ca93e459586924c6b33d27
Breon Knight <Breonknight7@gmail.com>
2015-12-06 21:03:46 -0800

    animes loaded, post functional

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,24 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
 
 // your code
+//array for anime storage
+var myAnimes = [];
+
+var $animeList = $('#anime-list');
+
+var $createAnime = $('#create-anime');
+
+ 
+var render = function() {
+
+	$animeList.empty();
+
+	var animeToHTML = {favAnimes: myAnimes};
+
+	$animeList.append(animeToHTML);
+}
+
+
 $.ajax({
 	type: 'GET',
 	url: 'https://murmuring-waters-9411.herokuapp.com/api/profile',
@@ -37,8 +55,46 @@ $.ajax({
 	type: 'GET',
 	url: 'https://murmuring-waters-9411.herokuapp.com/api/animes',
 	success: function(favAnimes){
-		console.log([favAnimes]);
+
+		favAnimes.favAnimes.forEach(function (element){
+			console.log(element);
+			$animeList.append("<ul class='list-group'>" + "<li class='list-group-item' data-id='" + element._id + "'>" + "Anime Title: " + element.title + "<br>"
+				+ "Genre: " + element.genre + "<br>" + "Finished: " + element.finished + "</li>" + "</ul>");
+		});
+		console.log([favAnimes.favAnimes.title]);
 	}
-})
+});
+
+$createAnime.on('submit', function (event){
+	event.preventDefault();
+	//allows me to save my anime title
+	var newAnime = $(this).serialize();
+
+	$.post('https://murmuring-waters-9411.herokuapp.com/api/animes', newAnime, function (favAnimes){
+		myAnimes.push(favAnimes.title);
+		console.log(favAnimes.title)
+
+		render();
+
+
+		});
 
 });
+
+});
+
+
+
+
+/*
+
+for (key in pInfo) {
+	if(obj.hasOwnProperty(key))
+	console.log(obj[key])
+	(#myanimes).on('click', '.delete', function(){
+	
+	$(this).data('id')
+	})
+}
+
+*/
