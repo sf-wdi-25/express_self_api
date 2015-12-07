0bd59fe0627b87658a3fa047d196586798fb9ea0
chan-d <chan.danny516@gmail.com>
2015-12-06 22:36:35 -0800

    basic page made on local machine

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,100 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+			var base_url= 'http://localhost:3000/';
+	  		var toAddUser; 
+	  		var toAddMessage;
+	  		
+	  		var toAddId= 0;
+	  		var toAdddata= {user: toAddUser, message: toAddMessage};
 
-// your code
+	$.ajax({
+		method: "GET",
+		url: '/api/profile',
+		success: function(element){
+			element.data.forEach(function (e){
+				var family = e.family_members;
+				$('.profile_target').append("<p>" + "<strong>Name : </strong>" + e.name + "</p>"+ "<p>" +"<strong>Github account : </strong>" + "<a href='http://github.com/chan-d/''>  chan-d</a>" + "</p>" + "<p><strong> Current city: </strong>" +e.current_city +"</p>"+ "<p><strong> Family Members: </strong>" + "</p>" + "<p>" + family[0].name + ": "+ family[0].relationship + "</p>"+ "<p>" + family[1].name + ": "+ family[1].relationship + "</p>"+ "<p>" + family[2].name + ": "+ family[2].relationship + "</p>");
 
+			});
+		}
+	});
+
+	$.ajax({
+		method: 'GET',
+		url: '/api/music',
+		success: function(data){
+			data.data.forEach(function (element){
+				$('.white').append("<p>"+ "<strong>Artist: </strong>" + element.artist + "</p>");
+			});
+
+		}
+	});
+
+	$.ajax({
+		method: 'GET',
+		url: 'http://localhost:3000/api/messages',
+		success: function getMessages (i) {
+			i.data.forEach(function (element){
+				$('.messages').append("<p> <strong> user: " + element.user + "</p>" + "<br>" + "<p> <strong> message: </strong>" + element.message + "</p>" + "<hr />");
+			});
+		}
+	});
+
+	$('#inputForm').on('submit', function (event) {
+			//parseInt(toAddId++);
+			event.preventDefault();
+	  		toAddUser= $('#name').val();
+	  		toAddMessage= $('#message').val();
+
+			var toAdddata= {user: toAddUser, message: toAddMessage};
+	  	$.ajax({
+	  		method: "POST",
+	  		url:'/api/messages',
+	  		data: toAdddata,
+	  		success: function(data){
+	  			$('.messages').append("<p> <strong> user: " + toAddUser + "</p>" + "<br>" + 
+	  								"<p> <strong> message: </strong>" + toAddMessage + "</p>" +
+	  								"<p> <button type='button' class='btn btn-primary'>Edit</button>" + "<button type='button' class='btn btn-danger'>Delete</button> </p> <hr />");	
+	  					$.ajax({
+         					method: 'GET',
+         					url:'api/messages',
+         				});
+	  		
+         	}	
+         });
+		
+	});
+	  		//res.json(messages);
+	  	
+      //    			$.ajax({
+      //    				method: 'GET',
+      //    				url: base_url + 'api/messages',
+      //    				success: function getMessages (i) {
+						// 	i.data.forEach(function (element){
+						// 			console.log(toAdddata);
+						// 		//$('.messages').append("<br><p>" + element + "</p>");
+						// 	});
+						// }
+      //    			}
+      //          		);
+      //          }
+	  			// element.forEach(function (e){
+	  			// 	$('messages').append(e);
+	  			// })
+	  // 			success: function(data) {
+	  // 		 	messages.append(data);
+	  // 			}
+			// });
+
+
+			
+
+
+	//});	
 });
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // require express and other modules
 var express = require('express'),
     app = express();
-
+var bodyParser = require('body-parser');
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -15,7 +18,23 @@ app.use(function(req, res, next) {
  * DATABASE *
  ************/
 
-// your hardcoded data here
+ //messages
+
+
+
+var music = [{
+        artist: 'Perfume'},
+        {artist: 'Calvin Harris'},
+        {artist: 'Dash'
+}];
+
+
+var messages = [{
+  _id: 0,
+  user: 'Danny',
+  message: 'Hello'
+}];
+
 
 /**********
  * ROUTES *
@@ -34,16 +53,70 @@ app.get('/', function homepage (req, res) {
  * JSON API Endpoints
  */
 
-app.get('/api', function api_index (req, res){
+app.get('/api/', function api_index (req, res) {
   res.json({
     message: "Welcome to my personal api!",
     documentation_url: "https://github.com/chan-d/express_self_api/README.md", // CHANGE THIS TO LINK TO YOUR README.md
     base_url: "https://frozen-bayou-5536.herokuapp.com/",
     endpoints: [
-      {method: "GET", path: "/api", description: "Describes available endpoints"}
+      {method: "GET", path: "/api", description: "My profile"}
     ]
-  })
+  });
 });
+
+app.get('/api/profile', function api_profile (req, res) {
+res.json({ 
+  message: "About Me",
+  base_url: "localhost:3000/",
+  data: [{
+      name: "Danny Chan",
+      github_link: "http://github.com/chan-d/",
+      github_profile_image: "",
+      current_city: "San Francisco",
+      family_members: [{name: 'Chris',
+                        relationship: 'Father',
+                      },
+                        {name: 'Celeste',
+                          relationship: 'Mother'
+                      },
+                        { name: 'Henry', 
+                          relationship: 'Brother',
+                      },]
+    }]
+  });
+});
+
+app.get('/api/music', function api_music(req, res){
+    res.json({
+        base_url: 'localhost:3000',
+        data: music
+    });
+});
+
+app.post('/api/messages', function post (req,res) {
+
+   
+    
+   // console.log('new message : '+ messages);
+     // if (messages.length > 0) {
+     //   messages._id = messages[messages.length] + 1;
+     // }else{
+     //   messages._id = messages._id + 1;
+     // }
+     var newMessage =req.body; 
+     messages.push(newMessage); 
+    //console.log("after new message added : " + messages);
+       res.send({data: newMessage});
+  
+  });
+
+  
+
+app.get('/api/messages', function api_messages(req, res){
+  res.json({data: messages});
+});
+
+
 
 /**********
  * SERVER *
