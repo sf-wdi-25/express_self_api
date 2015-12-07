4361a0d62a119484124982d92fd90ce3f0d3173e
revalidate <reva.bhatt@gmail.com>
2015-12-06 18:38:25 -0800

    updated server.js, index.html, app.js and style.css to incorporate functioning submit form & appended books on client side and GET, POST, & PUT on the server side

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,45 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-// your code
+	console.log('JS is loaded!'); // sanity check
+
+	$.ajax({
+		method:"GET",
+		url: "http://murmuring-chamber-7862.herokuapp.com/api/articles",
+		success: function(data) {
+   			data.articles.forEach(function(e) {
+   				var title = "<strong>Title: </strong>" + e.title;
+   				var author = "<strong>Author: <strong>" + e.author;
+   				var link = "<strong>Source: </strong><a href='"+ e.article_link + "'>Link</a>";
+   				var image = "<img src=" + e.image + ">";
+   				var editButton = "<button class='btn-primary'>" + "edit" + "</button>";
+   				var deleteButton = "<button class='btn-primary delete'>" + "delete" + "</button>";
+   				$("#index").append("<div class='articles'>" + "<div class='article-divs' id='article-image'>" + image + "</div>" + 
+   					"<div class='article-divs' id='article-list'><p>" + "<br>" + title + "<br>" + author + "<br>" + link + "<br>" + 
+   					editButton + deleteButton + "</p></div></div><hr />");
+   			});
+   		}
+	});
+
+	$("#submit").click(function(event) {
+		event.preventDefault();
+		$.ajax({
+	      method: "POST",
+	      url: "http://murmuring-chamber-7862.herokuapp.com/api/articles",
+	      data: {
+	        image: $("#subImage").val(),
+	        title: $("#subTitle").val(),
+	        link: $("#subLink").val(),
+	        link: $("#subAuthor").val()
+      	  },
+      	  success: function (response) {
+        	console.log("Added Article!");
+      	  },
+      	});
+	});	
+
+	$("#index").on("click", ".delete", function(event) {
+    	console.log("you're a genius");
+	});
 
 });
--- server.js
+++ server.js
@@ -16,7 +16,36 @@ app.use(function(req, res, next) {
  * DATABASE *
  ************/
 
-// your hardcoded data here
+// your hardcode here
+var profile = {
+  name: "Reva Bhatt",
+  current_city: "Cupertino",
+  github_link: "https://github.com/revalidate",
+  github_profile_image: "/images/tumblr.jpg"
+};
+
+var articles = [
+  {_id: 1, 
+    image: "/images/dubois-ambedkar-letter.jpg",
+    title: "What B.R. Ambedkar Wrote to W.E.B. Du Bois",
+    author: "",
+    article_link: "https://www.saada.org/tides/article/20140422-3553"
+  },
+
+  {_id: 2, 
+    image: "/images/jim-crow.png",
+    title: "The Aesthetic Beauty of Oppression: Jim Crow, Darkies, and Aunt Jemima",
+    author: "",
+    article_link: "https://www.academia.edu/6076346/The_Aesthetic_Beauty_of_Oppression_Jim_Crow_Darkies_and_Aunt_Jemima"
+  },
+
+    {_id: 3, 
+    image: "/images/black-asian-solidarity.jpg",
+    title: "Why Asian Americans Might Not Talk About Ferguson",
+    author: "",
+    article_link: "http://thesaltcollective.org/asian-americans-might-talk-ferguson/"
+  }
+];
 
 /**********
  * ROUTES *
@@ -46,11 +75,59 @@ app.get('/api', function api_index (req, res){
   })
 });
 
+app.get('/api/profile', function api_profile(req,res){
+  res.json(profile);
+});
+
+
+app.get('/api/articles', function index(req,res){
+  res.json({articles})
+});
+
+
+// app.get('/api/articles', function show(req,res){
+//   //show CRUD
+// });
+
+
+app.post('/api/articles', function create(req,res){
+  var newArticle = req.body;
+  if (articles.length > 0) {
+    newArticle._id = articles[articles.length - 1]._id + 1;
+  } else {
+    newArticle._id = 1;
+  }
+  articles.push(newArticle);
+  res.send(newArticle);
+});
+
+
+app.put('/api/articles', function update(req,res){
+  var newImage = req.body.image;
+  var newTitle = req.body.title;
+  var newLink = req.body.article_link;
+  var newId = parseInt(req.params.id);
+  var update = {_id: newId, image: newImage, title: newTitle, article_link: newLink};
+  res.json(update);
+});
+
+
+app.delete('/api/articles/:id', function destroy(req,res){
+  var deleteArticleId = parseInt(req.params.id);
+  var newArray = articles.find (function (e, i){
+    if (deleteArticleId === e._id) {
+      articles.splice(i, 1)
+      return articles;
+    }
+  });
+  res.send(newArray);
+});
+
 /**********
  * SERVER *
  **********/
 
 // listen on port 3000
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Express server is running on http://localhost:3000/');
+  console.log('Express server is running on http://murmuring-chamber-7862.herokuapp.com');
 });
