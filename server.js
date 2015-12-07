16bf0782f8f3d58746dd0f8e6b62da1696be4a92
Breon Knight <Breonknight7@gmail.com>
2015-12-06 22:13:49 -0800

    test

--- server.js
+++ server.js
@@ -93,9 +93,9 @@ app.get('/api/animes', function index(req, res) {
 
 app.post('/api/animes', function newAnime(req, res) {
   var anime = req.body;
-  console.log("The request: " + req);
-  console.log("The anime: " + anime);
-  if (favAnimes > 0) {
+  console.log("The request: " + JSON.stringify(req));
+  console.log("The anime: " + JSON.stringify(anime));
+  if (favAnimes > 0) {  
     anime._id = favAnimes[favAnimes.length - 1] + 1;
   }else{
     anime._id = 1;
