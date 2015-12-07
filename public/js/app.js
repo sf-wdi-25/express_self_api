1e17efc01beeb98b76bcf524a0fcf634355f881f
jroers <joe.roers@gmail.com>
2015-12-06 23:33:38 -0800

    replaced the append command with html to prevent multiplying information

--- public/js/app.js
+++ public/js/app.js
@@ -7,13 +7,13 @@ $(document).ready(function() {
   		method: "GET",
   		url: "/api/profile",
   		success: function (data) {
-  			$(".about-me").append("<h1><img src='" + data.github_profile_image + "'><br />" +
+  			$(".about-me").html("<h1><img src='" + data.github_profile_image + "'><br />" +
   				"About " + data.name + "</h1>" +
   				"<p>Chicago-native currently residing in " + data.current_city + ", and taking a web dev course in San Francisco.</p>");
   		},
   		error: function (error) {
   			console.log("Something went terribly wrong. Error code: " + error.status);
-  			$(".about-me").append("Oops! Looks like something went wrong. This data is currently unavailable.");
+  			$(".about-me").html("Oops! Looks like something went wrong. This data is currently unavailable.");
   		}
   	});
   });
