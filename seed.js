6d89e770006565d191b6ca822d862bc992a3ce4b
nathanallen <thenathanator@gmail.com>
2016-03-30 15:05:01 -0700

    stub seed.js with example

---
+++ seed.js
@@ -0,0 +1,5 @@
+// This file allows us to seed our application with data
+// simply run: `node seed.js` from the root of this project folder.
+
+// var db = require('./models');
+// db.Campsite.create({description: "Sharp rocks. Middle of nowhere."})
