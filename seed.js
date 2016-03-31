19aa8afd63398c72093430e88d99861cb4266658
nathanallen <thenathanator@gmail.com>
2016-03-31 14:42:03 -0700

    update seed task example

--- seed.js
+++ seed.js
@@ -2,4 +2,14 @@
 // simply run: `node seed.js` from the root of this project folder.
 
 // var db = require('./models');
-// db.Campsite.create({description: "Sharp rocks. Middle of nowhere."})
+
+// var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
+
+// db.Campsite.create(new_campsite, function(err, campsite){
+//   if (err){
+//     return console.log("Error:", err);
+//   }
+
+//   console.log("Created new campsite", campsite._id)
+//   process.exit(); // we're all done! Exit the program.
+// })
