b9f59ba160af89eac8f4195a36cef31c67f12ff9
nathanallen <thenathanator@gmail.com>
2016-07-15 09:34:54 -0700

    fixes #6 - add CORS header

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ var express = require('express'),
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// allow cross origin requests (optional)
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 /************
  * DATABASE *
  ************/
