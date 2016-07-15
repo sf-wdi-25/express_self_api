234ccecd9087f8f62126c658118704fd42b548d0
nathanallen <thenathanator@gmail.com>
2016-07-15 13:33:15 -0700

    fixes #7 update heroku mongolab connection string

--- models/index.js
+++ models/index.js
@@ -1,6 +1,4 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGOLAB_URI ||
-                  process.env.MONGOHQ_URL ||
-                  "mongodb://localhost/personal-api");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
 
 // module.exports.Campsite = require("./campsite.js.example");
