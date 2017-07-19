05036d057f8f5f76239ba889c12dfebe832dd3a1
Christopher Fujino <christopherfujino@gmail.com>
2017-07-19 14:42:47 -0700

    fixed: set useMongoClient setting to true, removes deprecation warning

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api", {useMongoClient: true});
 
 // module.exports.Campsite = require("./campsite.js.example");
